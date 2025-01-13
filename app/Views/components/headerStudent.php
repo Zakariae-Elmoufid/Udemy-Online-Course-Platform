c93fc3c9596252f36584f743e6b56febce090a68
zakariae elmoufid <moufidzakarie3@gmail.com>
2025-01-13 10:51:33 +0100

    acceses my courses

--- app/Views/components/headerStudent.php
+++ app/Views/components/headerStudent.php
@@ -1,19 +1,21 @@
 
-  <header class="bg-blue-600 text-white py-4">
+  <header class="bg-white text-fuchsia-600 py-4 border-fuchsia-900 border-b-2"">
         <div class="container mx-auto flex justify-between items-center px-6">
             <!-- Logo -->
             <h1 class="text-2xl font-bold">Udemy</h1>
             
             <!-- Navigation -->
-            <nav class="flex items-center space-x-6">
-                <a href="#my-courses" class="text-sm font-medium hover:text-gray-300">My Courses</a>
+            <nav class="flex items-center space-x-6 ">
                 <a href="#course-catalog" class="text-sm font-medium hover:text-gray-300">Course Catalog</a>
+                <a href="myCourses.php" class="text-sm font-medium hover:text-gray-300">My Courses</a>
                 <!-- Profile Icon -->
-                <button class="flex items-center space-x-2">
-                    <img src="../public/images/profil.jpg" alt="Profile" 
-                        class="w-10 h-10 rounded-full border-2 border-white">
-                    <span>Profile</span>
-                </button>
             </nav>
+            <button class="flex items-center space-x-2">
+                <img src="../public/images/profil.jpg" alt="Profile" 
+                    class="w-10 h-10 rounded-full border-2 border-fuchsia-800">
+                <span>Profile</span>
+                <i class='bx bx-chevron-down'></i>
+
+            </button>
         </div>
     </header>
